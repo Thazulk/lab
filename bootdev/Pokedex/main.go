3e954e7b1c3ded459013a39dca374cb192ee6f78
Thazulk <thazulk@gmail.com>
2025-01-27 15:54:36 +0100

    separate repl file

--- bootdev/Pokedex/main.go
+++ bootdev/Pokedex/main.go
@@ -1,20 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	// Create a single REPL
-	for {
-		userInput := bufio.NewScanner(os.Stdin)
-		fmt.Print("Pokedex > ")
-		userInput.Scan()
-		input := userInput.Text()
-		output := cleanInput(input)
-		fmt.Printf("Your command was: %s\n", output[0])
-
-	}
+	startRepl()
 }
