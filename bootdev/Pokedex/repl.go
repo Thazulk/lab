5ac35f9e4415a08cf36d0a1829317221e48ab097
Thazulk <thazulk@gmail.com>
2025-01-30 16:02:52 +0100

    help and exit mapping as command

--- bootdev/Pokedex/repl.go
+++ bootdev/Pokedex/repl.go
@@ -7,6 +7,27 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	for _, command := range getCommands() {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+	return nil
+}
+
 func startRepl() {
 	reader := bufio.NewScanner(os.Stdin)
 	for {
@@ -17,18 +38,45 @@ func startRepl() {
 		if len(words) == 0 {
 			continue
 		}
-		fmt.Printf("Your command was: %s\n", words[0])
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Printf("Unknown command")
+			continue
+		}
+
 	}
 }
 
 func cleanInput(text string) []string {
-	// var words []string
-	// for _, word := range strings.Fields(text) {
-	// 	words = append(words, strings.TrimSpace(word))
-	// }
-
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
-
 	return words
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		// "map": {
+		// 	name:        "map",
+		// 	description: "Display the map",
+		// 	callback:    commandMap,
+		// },
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
