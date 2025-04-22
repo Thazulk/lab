c21a024226930e11a308fca7a910700be4836d06
Thazulk <thazulk@gmail.com>
2025-04-22 22:15:55 +0200

    polyfills

---
+++ bootdev/heifer/main.compiled.js
@@ -0,0 +1,6 @@
+"use strict";
+
+function greet(name) {
+  console.log("moo, ".concat(name, " "));
+}
+greet("heifer");
--- bootdev/heifer/main.js
+++ bootdev/heifer/main.js
@@ -1,1 +1,4 @@
-console.log("moo!");
+function greet(name) {
+  console.log(`moo, ${name} `);
+}
+greet("heifer");
